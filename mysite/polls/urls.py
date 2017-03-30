fb66aaca092064b18cab7ac51c0b057ac5b17cee
kcmuths <kuppandachittiappa.m@husky.neu.edu>
2017-03-30 14:43:16 -0400

    views updated

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,8 +5,20 @@ from django.conf.urls import url
 
 from . import views ##importing views.py 
 
+app_name = 'polls'
 urlpatterns = [
+    #eg:/polls/
+
     url(r'^$', views.index, name='index'),
+
+    #eg: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
+
+    #eg:/polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = 'results'),
+
+    #eg:/polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
 ## urlpattern explanation:
@@ -19,3 +31,9 @@ urlpatterns = [
 Required arguments: url()argument:regex and url() argument:view. Django captures an HttpRequest object as the first argument 
 Check out page: https://docs.djangoproject.com/en/1.9/intro/tutorial01/ for more details
 """
+
+##NAMESPACING URL NAMES:
+"""  usually more than one app (poll) exists. So how does Django realize what to use when using
+{% url %} (check out detail.html or index.html page in templates directory)
+So the anser here is to add namespaces to URLconf. so hence we add the app_name to set
+the application namespace """
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.http import Http404
+
+# def index(request):
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+# 	template = loader.get_template('polls/index.html')
+# 	context = {
+# 		'latest_question_list': latest_question_list
+# 	}
+#     return HttpResponse(template.render(context, request))
+
+    ##NOTE: WE have seperated the templates, so above the 
+    ##code loads the template called /polls/index.html and passes it a context
+ 
+ ##WE CAN REWRITE THE ABOVE CODE WITH A SHORTCUT IN Django using render
+
 
 def index(request):
-    return HttpResponse("Hello World! Wou're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+   ##render returns a HttpResponse object of the given template     
 
 ##simplest view possible in Django
 ##Now, to call this view, it needs to be mapped to a url(for an url we need a
 ## URLconf)
 
 ##Hence to create a URLconf in the polls directory, we create the file urls.py
+
+##MORE VIEWS. views taking arguments
+## details view: Displays a question text, with no results but with form to vote
+
+##RAISING A 404 ERROR:
+
+# def detail(request, question_id):
+# 	try:
+# 		question = Question.objects.get(pk = question_id)
+# 	except Question.DoesNotExist:
+# 		raise Http404("Question does not exist")
+# 	return render(request, '/polls/detail.html', {'question:'})
+
+
+##EASIER and a SHORTCUT TO RAISE 404 ERROR:
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk = question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+
+##Results view page: Displays results for a particular question
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+## vote action view: handles voting for a particular question
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
+
+##NOTE: the page's design was hardcoded here. This needs to be seperated,
+##because otherwise we need to edit the python code. Hence we create the
+##DIRECTORY templates in the polls directory.
