eddc33b58da58fb00c6ff0aec9ee08e817f3cb11
kcmuths <kuppandachittiappa.m@husky.neu.edu>
2017-03-30 19:19:33 -0400

    adding forms to details

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Question
-from django.http import Http404
+from .models import Question, Choice
+from django.http import Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # def index(request):
 # 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -58,7 +59,20 @@ def results(request, question_id):
 
 ## vote action view: handles voting for a particular question
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message':"You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		##ALWAYS return a HttpResponseRedirect after successfully sealing with POST data. This prevents 
+		##data from being posted twice if user hits back button
+	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 ##NOTE: the page's design was hardcoded here. This needs to be seperated,
 ##because otherwise we need to edit the python code. Hence we create the
